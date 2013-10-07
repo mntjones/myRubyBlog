253da4325f4b279fed400211a7c421bb66cbf7b4
Monica Tamaru Jones <mntamaru@gmail.com>
2013-10-06 17:10:25 -0700

    heroku update

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'this is some sample text.';
+		@content_second = 'this is more text.';
+	end
+	
+	def new
+	
+	end
+	
+	def create
+	
+	end
+	
+	def edit
+	
+	end
+	
+	def update
+	
+	end
+	
+	def show
+	
+	end
+	
+	def destroy
+	
+	end
+	
+end
